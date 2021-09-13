eab2c36f1346c07772c6338011e09f2b2587cb02
Tanvi Punjani <tanupunjani@gmail.comi>
2021-09-13 01:22:41 -0400

    latest

--- app.py
+++ app.py
@@ -17,18 +17,18 @@ elif selectbox == 'run_app>':
 elif selectbox == 'about':
     show_about()
 
-if app_mode == "Show instructions":
-       run_intro()
-        st.sidebar.success('To continue select "Run the app".')
-    elif app_mode == "Source code":
-        t1 = st.title("Code:")
-        m1 = st.markdown(
-            "## [github link](https://github.com/tanupunjani/Emotion-Detection)"
-        )
-        code1 = st.code(get_file_content_as_string("app.py"))
-        title_1 = st.title("Functionality")
-        code2 = st.code(get_file_content_as_string("functionality.py"))
-    elif app_mode == 'Run the app':
-        # st.balloons()
-        run_the_app()
+# if app_mode == "Show instructions":
+#        run_intro()
+#     st.sidebar.success('To continue select "Run the app".')
+#     elif app_mode == "Source code":
+#         t1 = st.title("Code:")
+#         m1 = st.markdown(
+#             "## [github link](https://github.com/tanupunjani/Emotion-Detection)"
+#         )
+#         code1 = st.code(get_file_content_as_string("app.py"))
+#         title_1 = st.title("Functionality")
+#         code2 = st.code(get_file_content_as_string("functionality.py"))
+#     elif app_mode == 'Run the app':
+#         # st.balloons()
+#         run_the_app()
 
--- functionality.py
+++ functionality.py
@@ -2,26 +2,26 @@ import streamlit as st
 import numpy as np 
 import tensorflow_text as text 
 import tensorflow_hub as hub 
+import tensorflow as tf 
 import gdown
+import os
 
 model_url = "https://drive.google.com/uc?id=1--eULExMNhEKGiY4zZmdSB7dvMwh0nOX"
 model_path = './Best_model_emotion.h5'
 
 def load_model(model_path):
        # Don't download the file twice. (If possible, verify the download using the file length.)
-    if os.path.exists(file_path):
-        if os.path.getsize(file_path) >= 2770000:
+    if os.path.exists(model_path):
+        if os.path.getsize(model_path) >= 2770000:
             st.warning('model already there haha')
-            return
 
-    # These are handles to two visual elements to animate.
-    weights_warning, progress_bar = None, None
-    try:
-        weights_warning = st.warning("Downloading %s..." % file_path)
-        gdown.download(model_url, output=file_path)
-        st.warning('download finished')
-    finally:
-        st.write('thanks for the patience')
+    else:
+        try:
+            weights_warning = st.warning("Downloading %s..." % model_path)
+            gdown.download(model_url, output=model_path)
+            st.warning('download finished')
+        finally:
+            st.write('thanks for the patience')
     
     hub_layer = hub.KerasLayer("https://tfhub.dev/google/universal-sentence-encoder/4")
     model = tf.keras.models.load_model(model_path,custom_objects = {'KerasLayer':hub_layer})
@@ -33,10 +33,16 @@ def run_app():
     this is sample text 
     """)
 
-    model = load_model()
+    model = load_model(model_path = model_path)
 
-    prediction = model.predict(text_box_text)
-    st.write(prediction)
+    prediction = model.predict([text_box_text])
+    # st.write(prediction)
+    # print(prediction)
+
+    if round(prediction[0][0])==1: 
+        st.title("This is a negative sentence 😞")
+    else : 
+        st.title("This is a positive sentence 😉")
     return 
 
 
