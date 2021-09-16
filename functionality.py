8599c1f2a0c38581eb2a3d2ceb13ea259f494dd6
someshfengde <someshfengde@gmail.com>
2021-09-16 22:56:25 +0530

    adding firebase

--- functionality.py
+++ functionality.py
@@ -1,16 +1,30 @@
-import streamlit as st 
-import numpy as np 
-import tensorflow_text as text 
-import tensorflow_hub as hub 
-import tensorflow as tf 
+from google.cloud import firestore
+import streamlit as st
+import numpy as np
+import tensorflow_text as text
+import tensorflow_hub as hub
+import tensorflow as tf
 import gdown
 import os
 
 model_url = "https://drive.google.com/uc?id=1--eULExMNhEKGiY4zZmdSB7dvMwh0nOX"
 model_path = './Best_model_emotion.h5'
 
+
+def store_pred(text, pred, val):
+    db = firestore.Client.from_service_account_json(
+        'streamlit-store-firebase-adminsdk-9gtlx-9f79330f32.json')
+    data = {
+        "pred": pred,
+        "prediction_str": val,
+        "text": text
+    }
+    doc_ref = db.collection("text_pred_tab_0").document()
+    doc_ref.set(data, merge=True)
+
+
 def load_model(model_path):
-       # Don't download the file twice. (If possible, verify the download using the file length.)
+    # Don't download the file twice. (If possible, verify the download using the file length.)
     if os.path.exists(model_path):
         if os.path.getsize(model_path) >= 2770000:
             st.warning('model already there haha')
@@ -22,10 +36,12 @@ def load_model(model_path):
             st.warning('download finished')
         finally:
             st.write('thanks for the patience')
-    
-    hub_layer = hub.KerasLayer("https://tfhub.dev/google/universal-sentence-encoder/4")
-    model = tf.keras.models.load_model(model_path,custom_objects = {'KerasLayer':hub_layer})
-    return model 
+
+    hub_layer = hub.KerasLayer(
+        "https://tfhub.dev/google/universal-sentence-encoder/4")
+    model = tf.keras.models.load_model(
+        model_path, custom_objects={'KerasLayer': hub_layer})
+    return model
 
 
 def run_app():
@@ -33,16 +49,18 @@ def run_app():
     this is sample text 
     """)
 
-    model = load_model(model_path = model_path)
+    model = load_model(model_path=model_path)
 
     prediction = model.predict([text_box_text])
     # st.write(prediction)
     # print(prediction)
 
-    if round(prediction[0][0])==1: 
+    if round(prediction[0][0]) == 1:
+        val = "neg"
         st.title("This is a negative sentence 😞")
-    else : 
+    else:
         st.title("This is a positive sentence 😉")
-    return 
+        val = "pos"
 
-
+    store_pred(text=text_box_text, pred=prediction[0][0], val=val)
+    return
